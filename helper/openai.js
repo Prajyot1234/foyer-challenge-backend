9fd523ae229a3f88b71823565257279e32910fd4
Prajyot Burbure <59968235+Prajyot1234@users.noreply.github.com>
2023-07-10 12:40:30 +0530

    Update openai.js

--- helper/openai.js
+++ helper/openai.js
@@ -1,8 +1,8 @@
 const { Configuration, OpenAIApi } = require("openai");
 
 const config = new Configuration({
-    organization :  "org-b3hYMUUFQugmpniLVV9DbYnE",
-    apiKey : "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"
+    organization :  "org-b3hYMUUFQugmpniLVV9DbYnE", // for demonstration purpose given this key will delete after some time
+    apiKey : "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"  // for demonstration purpose given this key will delete after some time 
 });
 
 const openai = new OpenAIApi(config);
